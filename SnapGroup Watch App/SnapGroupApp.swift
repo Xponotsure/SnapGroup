3930edce91e4b28dee0c5773b737e4456890f48a
Ayatullah Ma'arif <aya.mf12@gmail.com>
2024-06-19 14:10:02 +0700

    Initial Commit

---
+++ SnapGroup Watch App/SnapGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapGroupApp.swift
+//  SnapGroup Watch App
+//
+//  Created by Ayatullah Ma'arif on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SnapGroup_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnapGroup/SnapGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapGroupApp.swift
+//  SnapGroup
+//
+//  Created by Ayatullah Ma'arif on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SnapGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
