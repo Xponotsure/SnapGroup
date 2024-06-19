ee3a45376d6a2d3f01422ad45f70de2ddc240a2b
Ayatullah Ma'arif <aya.mf12@gmail.com>
2024-06-19 20:28:26 +0700

    Squashed commit of the following:
    commit a40b1455bcd4f6be8a4b46d883dde320b0d69d84
    Merge: 94e2d16 11d88cd
    Author: channylim24 <86568397+channylim24@users.noreply.github.com>
    Date:   Wed Jun 19 15:36:46 2024 +0700
    
        Merge pull request #3 from Xponotsure/dev/channy
    
        Add: flash, landscape portrait orientation, tap to focus
    
    commit 11d88cda7b7a857d33ff3c7850fd038c1103c94b
    Author: channylim24 <86568397+channylim24@users.noreply.github.com>
    Date:   Wed Jun 19 15:33:00 2024 +0700
    
        chore: watch dependency
    
    commit dde5165a40e1b2f6ba8eb2184a890b1b5d15f6c2
    Author: channylim24 <86568397+channylim24@users.noreply.github.com>
    Date:   Wed Jun 19 15:28:28 2024 +0700
    
        Squashed commit of the following:
    
        commit 94e2d161548da4a36c966512a1b4242f2f66fab6
        Merge: 47a27e1 d916d80
        Author: Ayatullah Ma'arif <88651324+ayckermann@users.noreply.github.com>
        Date:   Wed Jun 19 14:55:23 2024 +0700
    
            Merge pull request #2 from Xponotsure/dev-aya
    
            Update gitignore and minimum deployment version
    
        commit d916d80a258b8fde8504bf16893ac72119b2404b
        Merge: 5de40ca 557fa6a
        Author: Ayatullah Ma'arif <aya.mf12@gmail.com>
        Date:   Wed Jun 19 14:52:50 2024 +0700
    
            Merge branch 'dev-aya' of https://github.com/Xponotsure/SnapGroup into dev-aya
    
        commit 5de40ca49019f00149be8781f329638c38485605
        Author: Ayatullah Ma'arif <aya.mf12@gmail.com>
        Date:   Wed Jun 19 14:52:47 2024 +0700
    
            Update xcschememanagement.plist
    
        commit 135c41492396823b8b3b94b121b3e66421da5461
        Author: Ayatullah Ma'arif <aya.mf12@gmail.com>
        Date:   Wed Jun 19 14:50:02 2024 +0700
    
            chore: add gitignore and update minimum deployment
    
        commit 557fa6ab1119834f5ef74aa08b79f3406c8d338d
        Author: Ayatullah Ma'arif <aya.mf12@gmail.com>
        Date:   Wed Jun 19 14:50:02 2024 +0700
    
            chore: add gitignore and update minimum deployment
    
    commit a0622429a3ec3d4c0687fdddf89fe7f21e13fc68
    Author: channylim24 <86568397+channylim24@users.noreply.github.com>
    Date:   Wed Jun 19 15:27:53 2024 +0700
    
        Add: flash, landscape portrait orientation, tap to focus
    
    commit 94e2d161548da4a36c966512a1b4242f2f66fab6
    Merge: 47a27e1 d916d80
    Author: Ayatullah Ma'arif <88651324+ayckermann@users.noreply.github.com>
    Date:   Wed Jun 19 14:55:23 2024 +0700
    
        Merge pull request #2 from Xponotsure/dev-aya
    
        Update gitignore and minimum deployment version

---
+++ SnapGroup/Camera/Extensions/ControlButtonView.swift
@@ -0,0 +1,28 @@
+//
+//  ControlButtonView.swift
+//  rotateCamera
+//
+//  Created by Channy Lim on 17/06/24.
+//
+
+import SwiftUI
+
+struct ControlButtonView: View {
+    let label: String
+    let action: () -> Void
+    
+    var body: some View {
+        Button {
+            action()
+        } label: {
+            Text(label)
+                .tint(.white)
+                .font(.title3)
+                .fontWeight(.semibold)
+        }
+    }
+}
+
+#Preview {
+    ControlButtonView(label: "Cancel", action: {})
+}
